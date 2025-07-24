9be8060197c6c182c173b04f757da549f2edb302
FloLeh <florianlehuede@gmail.com>
2025-07-24 17:16:52 +0200

    bootstrap

--- src/main/java/com/openclassrooms/poseidon/domain/BidList.java
+++ src/main/java/com/openclassrooms/poseidon/domain/BidList.java
@@ -3,6 +3,7 @@ package com.openclassrooms.poseidon.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,6 +28,7 @@ public class BidList implements DomainEntity<BidList> {
     @NotBlank
     private String type;
 
+    @NotNull
     @Positive
     private Double bidQuantity;
 
